3ae8d771260427fd937ff207f4a7e0a69d111ab3
LRojon <loic.rojon@gmail.com>
2021-01-19 16:40:45 +0100

    Add Mode tiles (design)

--- index.php
+++ index.php
@@ -14,29 +14,35 @@
     <script src="js/colorpicker.min.js"></script>
     <title>Control Yeelight</title>
     <style>
-        *{
-            border: 1px solid red;
+        .col-6{
+            border: solid 2px #1a1a1a;
         }
     </style>
 </head>
-<body class="bg-dark" style="width: fit-content; height: max-content;">
+<body class="bg-primary">
     <div class="container">
-        <div class="row" style="margin: 5%;">
-            <div class="col-6 text-center">
-                <button class="btn btn-primary"><h3>Mode</h3></button>
+        <div class="row">
+            <div class="col-6 text-center bg-success">
+                <h3 class="text-white">Mode</h3> <br>
+                <select name="mode" id="mode" class="form-control mb-2">
+                    <option value="off">OFF</option>
+                    <option value="normal">Normal</option>
+                    <option value="color">Pas d'automatisme</option>
+                    <option value="print">Impression</option>
+                </select>
             </div>
-            <div class="col-6 text-center">
-                <h3 class="text-white">Automatisation</h3> <br>
+            <div class="col-6 text-center bg-danger">
+                <h3 class="text-white">Automate</h3> <br>
                 <span class="text-white">WIP</span>
             </div>
         </div>
-        <div class="row" style="margin: 5%;">
-            <div class="col-6 text-center">
+        <div class="row">
+            <div class="col-6 text-center bg-dark">
                 <h3 class="text-white">Couleur</h3> <br>
                 <canvas id="color-picker"></canvas>
                 <input id="color" class="form-control">
             </div>
-            <div class="col-6 text-center">
+            <div class="col-6 text-center bg-info">
                 <h3 class="text-white">Bright</h3> <br>
                 <span class="text-white" id="valBright">0 %</span>
                 <input type="range" class="custom-range" name="bright" id="bright" min=1 max=100></div>
@@ -48,6 +54,7 @@
     let width = window.innerWidth;
     let height = window.innerHeight;
     $("#valBright").html($("#bright").val() + " %");
+    $("#mode").val("off");
 
     let colorPick = new KellyColorPicker({
         place : 'color-picker',
@@ -62,12 +69,21 @@
         }
     });
 
-    $(".container").width = width;
-    $(".container").height = height;
-    $(".col-6").height = height / 2;
-
     $("#bright").change(function(){
         $("#valBright").html($("#bright").val() + " %");
     });
+
+    $("#mode").change(function(){
+        switch ($("#mode").val()) {
+            case "off":
+                break;
+            case "normal":
+                break;
+            case "color":
+                break;
+            case "print":
+                break;
+        }
+    })
 </script>
 </html>
